1f085d148213870d8e2df1ee30b4f80e4f5245ea
srishti-23 <142992320+srishti-23@users.noreply.github.com>
2024-06-18 19:02:26 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: 'http://localhost:5174',
+    origin: true,
     credentials: true
 };
 app.use(cors(corsOptions));
